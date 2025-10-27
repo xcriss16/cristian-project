3bc4b6c53d896c201a0aaadeda507f634a27f365
xcriss16 <1922965@alu.murciaeduca.es>
2025-10-27 20:55:40 +0100

    Creacion clase producto

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+## creamos la clase productos
+class producto:
+
+    ## vamos a definir los atributos de mis productos
+    def __init__(self, nombre, precio, stock):
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock  
+
+        ## creamos una manzana que tenga de precio 1.5 y que hayan 10 en stock
+        mi_producto1 =  ("Manzana",1.5,10)
+        # vamos a mostrar algunas de las caracteristicas de mi producto creado
+        print(mi_producto1.nombre)
+        print(mi_producto1.precio)
+        print(mi_producto1.stock)
